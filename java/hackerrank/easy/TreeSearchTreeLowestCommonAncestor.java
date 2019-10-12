4242ba03f345001dc463bb3305839f0b4217dacf
elendili <elendilab@gmail.com>
2019-10-12 20:17:56 +0300

    + add recursive search of lowest common ancestor

--- java/hackerrank/easy/TreeSearchTreeLowestCommonAncestor.java
+++ java/hackerrank/easy/TreeSearchTreeLowestCommonAncestor.java
@@ -3,7 +3,9 @@ package hackerrank.easy;
 import hierarchy.Node;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 import static hierarchy.TreeUtils.getTreeRootFromSpacedString;
 import static org.junit.Assert.assertEquals;
@@ -41,41 +43,66 @@ public class TreeSearchTreeLowestCommonAncestor {
                     break;
                 }
             }
-            n = node.left;
-            if (n != null) {
-                nodes.addFirst(n);
-                backReferences.put(n, node);
+            if (!(node.data > v1 && node.data > v2)) {
+                n = node.right;
+                if (n != null) {
+                    nodes.addFirst(n);
+                    backReferences.put(n, node);
+                }
             }
-            n = node.right;
-            if (n != null) {
-                nodes.addFirst(n);
-                backReferences.put(n, node);
+            if (!(node.data > v1 && node.data > v2)) {
+                n = node.left;
+                if (n != null) {
+                    nodes.addFirst(n);
+                    backReferences.put(n, node);
+                }
             }
         }
         LinkedList<Node> path1 = backTrace(v1Node, backReferences);
         LinkedList<Node> path2 = backTrace(v2Node, backReferences);
-        Node out=null;
+        Node out = null;
         while ((!path1.isEmpty() && !path2.isEmpty()) && path1.peekFirst().equals(path2.peekFirst())) {
-            out=path1.pollFirst();
+            out = path1.pollFirst();
             path2.pollFirst();
         }
 
         return out;
     }
 
+    public static Node recursiveLca(Node root, int v1, int v2) {
+        if (root == null) {
+            return null;
+        }
+        if (root.data > v1 && root.data > v2) {
+            return recursiveLca(root.left, v1, v2);
+        }
+        if (root.data < v1 && root.data < v2) {
+            return recursiveLca(root.right, v1, v2);
+        }
+        return root;
+    }
 
     @Test
-    public void test() {
-        Node a = lca(getTreeRootFromSpacedString("4 2 3 1 7 6"), 1, 7);
+    public void testIterable() {
+        cases(TreeSearchTreeLowestCommonAncestor::lca);
+    }
+
+    @Test
+    public void tesRecursive() {
+        cases(TreeSearchTreeLowestCommonAncestor::recursiveLca);
+    }
+
+    @FunctionalInterface
+    public interface FunctionUnderTest<T, U, R> {
+        R apply(T t, U u1, U u2);
+    }
+
+    public void cases(FunctionUnderTest<Node, Integer, Node> functionUnderTest) {
+        Node a = functionUnderTest.apply(getTreeRootFromSpacedString("1 2"), 1, 2);
+        assertEquals(1, a.data);
+        a = functionUnderTest.apply(getTreeRootFromSpacedString("4 2 3 1 7 6"), 1, 7);
         assertEquals(4, a.data);
     }
-
-    @Test
-    public void test2() {
-        Node a = lca(getTreeRootFromSpacedString("1 2"), 1, 2);
-        assertEquals(1, a.data);
-    }
-
 }
 /*
 
