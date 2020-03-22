960101af68630ce1bf7c6968897b69a8f0d66bf7
elendili <elendilab@gmail.com>
2020-03-23 00:06:50 +0300

    + Trie

---
+++ java/leetcode/Trie.java
@@ -0,0 +1,82 @@
+package leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Trie {
+
+    static class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean leaf = false;
+    }
+
+    TrieNode root = new TrieNode();
+
+    /**
+     * Inserts a word into the trie.
+     */
+    public void insert(String word) {
+        char[] ca = word.toCharArray();
+        TrieNode current = root;
+        for (int i=0;i<ca.length;i++) {
+            int childIndex = ca[i] - 97;
+            TrieNode curChild = current.children[childIndex];
+            TrieNode next;
+            if(curChild!=null){
+                next=curChild;
+            } else {
+                next = new TrieNode();
+            }
+            current.children[childIndex] = next;
+            current = next;
+        }
+        current.leaf = true;
+    }
+
+    /**
+     * Returns if the word is in the trie.
+     */
+    public boolean search(String word) {
+        TrieNode tn =  getLastNode(word);
+        return tn!=null && tn.leaf;
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        return getLastNode(prefix)!=null;
+    }
+
+    private TrieNode getLastNode(String prefix){
+        char[] ca = prefix.toCharArray();
+        TrieNode current = root;
+        for (int i=0;i<ca.length;i++) {
+            TrieNode next = current.children[ca[i] - 97];
+            if (next != null) {
+                current = next;
+            } else {
+                return null;
+            }
+        }
+        return current;
+    }
+    @Test
+    public void test(){
+        Trie trie = new Trie();
+        trie.insert("app");
+        Assertions.assertTrue(trie.search("app"));
+        Assertions.assertFalse(trie.search("appp"));
+        Assertions.assertFalse(trie.search("ap"));
+        trie.insert("apple");
+        trie.insert("add");
+        Assertions.assertFalse(trie.search("apps"));
+        Assertions.assertTrue(trie.search("app"));
+        Assertions.assertTrue(trie.search("add"));
+        Assertions.assertFalse(trie.search("ad"));
+
+        Assertions.assertTrue(trie.startsWith("ad"));
+        Assertions.assertTrue(trie.startsWith("add"));
+        Assertions.assertTrue(trie.startsWith("a"));
+    }
+}
