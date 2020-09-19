d18f788ec0bc258f7128d18fea66f67689ae9fd7
elendili <elendilab@gmail.com>
2020-09-19 20:28:42 +0300

    +2

---
+++ java/leetcode/top_interview_questions/medium/dynamicProgramming/CoinChange.java
@@ -0,0 +1,127 @@
+package leetcode.top_interview_questions.medium.dynamicProgramming;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
+
+/*
+https://leetcode.com/explore/featured/card/top-interview-questions-medium/111/dynamic-programming/809/
+You are given coins of different denominations and a total amount of money amount.
+Write a function to compute the fewest number of coins that you need to make up that amount.
+If that amount of money cannot be made up by any combination of the coins, return -1.
+
+Example 1:
+Input: coins = [1, 2, 5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+
+Example 2:
+Input: coins = [2], amount = 3
+Output: -1
+Note:
+You may assume that you have an infinite number of each kind of coin.
+ */
+public class CoinChange {
+    private static void helper(final int[] coins,
+                               final int coinIndex,
+                               final int amount,           // how much left to exchange
+                               final int accumulatedCount, // how much coins we have in current recursion branch
+                               final int[] result) {
+        if (coinIndex > -1) {
+            int count = amount / coins[coinIndex];
+            final int rem = amount % coins[coinIndex];
+            if (rem == 0) {
+                int toPut = accumulatedCount + count;
+                result[0] = result[0] < 0? toPut : Math.min(result[0], toPut);
+                return;
+            }
+            while (count > -1
+                    &&
+                    (result[0]<0
+                    ||
+                    // check if final result is already better than can be found in this loop
+                    (accumulatedCount + count + 1) < result[0])) {
+                helper(coins, coinIndex - 1, amount - coins[coinIndex] * count, accumulatedCount + count, result);
+                count--; // let's try with less count
+            }
+        }
+    }
+
+    public static int coinChange(final int[] coins, final int amount) {
+        Arrays.sort(coins);
+        int[] result = new int[]{-1};
+        helper(coins, coins.length - 1, amount, 0, result);
+        return result[0];
+    }
+
+    /*
+        perf  complexity O(S*n)
+        space complexity O(n)
+     */
+    public static int coinChangeDP(int[] coins, final int amount) {
+        if (coins == null || coins.length == 0 || amount < 0) {
+            return -1;
+        }
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; i++) {
+            int best = dp[i];
+            for (int j = coins.length - 1; j > -1; j--) {
+                int coin = coins[j];
+                if (coin <= i) {
+                    best = Math.min(best, dp[i - coin] + 1);
+                }
+            }
+            dp[i] = best;
+        }
+        int out = dp[amount];
+        out = (out > amount) ? -1 : out;
+        return out;
+    }
+
+    @ParameterizedTest(name = "{index}. {0}")
+    @MethodSource("predicateStream")
+    public void test(String name, BiFunction<int[], Integer, Integer> methodUnderTest) {
+        Assertions.assertEquals(3, methodUnderTest.apply(new int[]{1, 2, 5}, 11));
+        Assertions.assertEquals(2, methodUnderTest.apply(new int[]{1, 2, 5}, 10));
+        Assertions.assertEquals(5, methodUnderTest.apply(new int[]{1, 2}, 10));
+        Assertions.assertEquals(7, methodUnderTest.apply(new int[]{1}, 7));
+        Assertions.assertEquals(3, methodUnderTest.apply(new int[]{1, 3, 5}, 7));
+    }
+
+    @ParameterizedTest(name = "{index}. {0}")
+    @MethodSource("predicateStream")
+    public void negTest(String name, BiFunction<int[], Integer, Integer> methodUnderTest) {
+        Assertions.assertEquals(-1, methodUnderTest.apply(new int[]{3}, 10));
+        Assertions.assertEquals(-1, methodUnderTest.apply(new int[]{2}, 3));
+    }
+
+    @ParameterizedTest(name = "{index}. {0}")
+    @MethodSource("predicateStream")
+    public void giantTest(String name, BiFunction<int[], Integer, Integer> methodUnderTest) {
+        Assertions.assertEquals(-1, methodUnderTest.apply(new int[]{Integer.MAX_VALUE}, Integer.MAX_VALUE / 100));
+        Assertions.assertEquals(1, methodUnderTest.apply(new int[]{Integer.MAX_VALUE / 100}, Integer.MAX_VALUE / 100));
+    }
+
+    @ParameterizedTest(name = "{index}. {0}")
+    @MethodSource("predicateStream")
+    public void test2(String name, BiFunction<int[], Integer, Integer> methodUnderTest) {
+        Assertions.assertEquals(20, methodUnderTest.apply(new int[]{186, 419, 83, 408}, 6249));
+    }
+
+    public static Stream<Arguments> predicateStream() {
+        return Stream.of(
+                Arguments.arguments("coinChangeDP",
+                        (BiFunction<int[], Integer, Integer>) (tn, amount) -> coinChangeDP(tn, amount)),
+                Arguments.arguments("coinChange",
+                        (BiFunction<int[], Integer, Integer>) (tn, amount) -> coinChange(tn, amount))
+
+        );
+    }
+}
---
+++ java/leetcode/top_interview_questions/medium/dynamicProgramming/LongestIncreasingSubsequence.java
@@ -0,0 +1,59 @@
+package leetcode.top_interview_questions.medium.dynamicProgramming;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/*
+https://leetcode.com/explore/featured/card/top-interview-questions-medium/111/dynamic-programming/810/
+Given an unsorted array of integers, find the length of longest increasing subsequence.
+
+Example:
+Input: [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Note:
+There may be more than one LIS combination, it is only necessary for you to return the length.
+Your algorithm should run in O(n2) complexity.
+Follow up: Could you improve it to O(n log n) time complexity?
+ */
+public class LongestIncreasingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+        int[] seqs = new int[nums.length];
+        int l = 0;
+        int seqsIndex;
+        for (int i = 0; i < nums.length; i++) {
+            int v = nums[i];
+            seqsIndex = Arrays.binarySearch(seqs, 0, l, v);
+            if (seqsIndex < 0) {
+                seqsIndex = -(seqsIndex + 1);
+            }
+            l = Math.max(l, seqsIndex + 1);
+            seqs[seqsIndex] = v;
+        }
+        return l;
+    }
+
+    @Test
+    public void downTest() {
+        Assertions.assertEquals(1, lengthOfLIS(new int[]{3, 2, 1}));
+        Assertions.assertEquals(1, lengthOfLIS(new int[]{-1, -2, -3}));
+    }
+
+    @Test
+    public void upTest() {
+        Assertions.assertEquals(3, lengthOfLIS(new int[]{1, 2, 3}));
+        Assertions.assertEquals(3, lengthOfLIS(new int[]{-3, -2, -1}));
+    }
+
+    @Test
+    public void test() {
+        Assertions.assertEquals(4, lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+        Assertions.assertEquals(3, lengthOfLIS(new int[]{10, 1, 9, 2, 8, 3}));
+        Assertions.assertEquals(3, lengthOfLIS(new int[]{100, 1, 101, 2, 102, 3}));
+        Assertions.assertEquals(3, lengthOfLIS(new int[]{100, 1, 101, 2, 102, 3}));
+        Assertions.assertEquals(2, lengthOfLIS(new int[]{100, 0, 0, 0, 0, 101}));
+    }
+
+}
