c70b15ad543050a80d911de153f50dc7fa337424
elendili <elendilab@gmail.com>
2020-09-19 22:26:59 +0300

    +SerializeAndDeserializeBinaryTree.java

---
+++ java/leetcode/top_interview_questions/medium/design/SerializeAndDeserializeBinaryTree.java
@@ -0,0 +1,119 @@
+package leetcode.top_interview_questions.medium.design;
+
+import leetcode.top_interview_questions.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/*
+https://leetcode.com/explore/featured/card/top-interview-questions-medium/112/design/812/
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored
+in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary tree.
+There is no restriction on how your serialization/deserialization algorithm should work.
+You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+Clarification: The input/output format is the same as how LeetCode serializes a binary tree.
+You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+
+
+
+Example 1:
+
+Input: root = [1,2,3,null,null,4,5]
+Output: [1,2,3,null,null,4,5]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+Input: root = [1,2]
+Output: [1,2]
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 10^4].
+-1000 <= Node.val <= 1000 (2^10)
+
+
+ */
+public class SerializeAndDeserializeBinaryTree {
+
+    // Your Codec object will be instantiated and called as such:
+    // Codec ser = new Codec();
+    // Codec deser = new Codec();
+    // TreeNode ans = deser.deserialize(ser.serialize(root));
+
+    /*
+    idea:
+    max val is less than 2^10
+    tree size is 10^4 ~ 2^14
+    char size is 2^16
+    can we store left/right in char?
+     */
+    // Encodes a tree to a single string.
+    static final char NULL = '#';
+
+    public String serialize(TreeNode root) {
+        return serial(root, new StringBuilder()).toString();
+    }
+
+    private StringBuilder serial(TreeNode n, StringBuilder sb) {
+        if (n == null) {
+            sb.append(NULL);
+        } else {
+            sb.append(intToChar(n.val));
+            serial(n.left, sb);
+            serial(n.right, sb);
+        }
+        return sb;
+    }
+
+    private char intToChar(int i) {
+        return (char) ('0' + 1000 + i);
+    }
+
+    private int charToInt(char c) {
+        return (int) (c - 1000 - '0');
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        return deserial(data.toCharArray(), new int[]{0});
+    }
+
+    private  TreeNode deserial(char[] chars, int[] index) {
+        char v = chars[index[0]++];
+        if (NULL==v) {
+            return null;
+        } else {
+            TreeNode n = new TreeNode(charToInt(v));
+            n.left = deserial(chars, index);
+            n.right = deserial(chars, index);
+            return n;
+        }
+    }
+
+    @Test
+    public void test() {
+        TreeNode input = TreeNode.from(1, 2, 3, 4, 5, 6, 7, null, 8, 9, 10, 11, 1000, 900);
+        String serialized = serialize(input);
+        System.out.println(serialized);
+        Assertions.assertEquals(input, deserialize(serialized));
+    }
+
+    @Test
+    public void negValues() {
+        TreeNode input = TreeNode.from(-1, -2, -3, -4, -5, -6, -7);
+        String serialized = serialize(input);
+        System.out.println(serialized);
+        Assertions.assertEquals(input, deserialize(serialized));
+    }
+
+}
