02a4b335d654c0ab3f94b19f0f135a1c11a40008
elendili <elendilab@gmail.com>
2021-02-02 00:41:59 +0300

    + SubarraySumsDivisibleByK.java

--- java/leetcode/medium/SubarraySumsDivisibleByK.java
+++ java/leetcode/medium/SubarraySumsDivisibleByK.java
@@ -15,24 +15,22 @@ public class SubarraySumsDivisibleByK {
         int[] remCount = new int[k];
         // get cumulative sum, take reminder of this cumulative sum
         // and increment correspondent mod[] cell
-        int cumSum = 0;
+        int cumSumReminder = 0;
+        int out = 0;
         for (int i : a) {
-            cumSum += i;
+            cumSumReminder += i;
+            cumSumReminder %= k; // convert sum to reminder
+            if (cumSumReminder < 0) { // tackle negative sum
+                cumSumReminder += k;
+            }
             // sum can be negative resulting in negative reminder
             // here I learnt that reminder can't be negative and
             // what java gives me is nonsense, so need to shift
             // reminder by dividend and get reminder again.
             // short: add k to negative reminder and get reminder again
-            int modIndex = (cumSum % k + k) % k;
-            remCount[modIndex]++;
-        }
-        int out = 0;
-        for (int rem : remCount) {
-            if (rem > 1) {
-                // get sub-arrays count for reminder
-                int add = rem * (rem - 1) / 2; // Gauss sum
-                out += add;
-            }
+//            int modIndex = (cumSum % k + k) % k;
+            out += remCount[cumSumReminder]; // add all previous arrays
+            remCount[cumSumReminder]++;
         }
         out += remCount[0];
         return out;
