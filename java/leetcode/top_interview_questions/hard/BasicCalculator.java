3dc9a48715bef2cbde4dd27308bce6d91b449dcc
elendili <elendilab@gmail.com>
2022-05-09 20:28:12 +0200

    + BasicCalculator

--- java/leetcode/top_interview_questions/hard/BasicCalculator.java
+++ java/leetcode/top_interview_questions/hard/BasicCalculator.java
@@ -2,9 +2,6 @@ package leetcode.top_interview_questions.hard;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayDeque;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -19,83 +16,61 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Every number and running calculation will fit in a signed 32-bit integer.
  */
 public class BasicCalculator {
-
-    public int calculate(String s) {
-        ArrayDeque<Object> tokens = tokenize(s);
-        calculate(tokens);
-        return (int) tokens.getFirst();
+    int charIndex = 0;
+    char[] s;
+    StringBuilder numberAccum;
+    public int calculate(String string) {
+        charIndex = 0;
+        s = string.toCharArray();
+        numberAccum = new StringBuilder();
+        return calcRecursive();
     }
 
-    public void calculate(ArrayDeque<Object> tokens) {
-        int out = 0;
-        Object t;
+    public int calcRecursive() {
+        int leftV = 0;
+        int rightV;
+        char c;
         final char defaultCurOp = '+';
         char curOp = defaultCurOp;
-        while (!tokens.isEmpty()
-                && !(t = tokens.pollFirst()).equals(')')) {
-            if (t.equals('(')) {
-                calculate(tokens);
-                t = tokens.pollFirst();
-            }
-            if (t instanceof Integer) {
-                if (curOp == '+') {
-                    out = out + ((Integer) t);
-                } else {
-                    out = out - ((Integer) t);
-                }
-                curOp=defaultCurOp;
+
+        while (charIndex < s.length
+                && (c = s[charIndex++]) != ')') {
+            if (c == '(') {
+                rightV = calcRecursive();
+                leftV = applyCurrentOperation(curOp,leftV,rightV);
+                curOp = defaultCurOp;
+            } else if (c == '+' || c == '-') {
+                rightV = getNumberFromAccumulator();
+                leftV = applyCurrentOperation(curOp,leftV,rightV);
+                curOp = c;
+            } else if (c == ' ') {
+                // skip
             } else {
-                curOp = (char) t;
+                numberAccum.append(c);
             }
         }
-        tokens.push(out);
+
+        rightV = getNumberFromAccumulator();
+        leftV = applyCurrentOperation(curOp,leftV,rightV);
+
+        return leftV;
     }
 
-    ArrayDeque<Object> tokenize(String s) {
-        ArrayDeque<Object> out = new ArrayDeque<>();
-        StringBuilder digAcc = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (c == ' ') {
-                //skip
-            } else if (c == '-') {
-                if (!digAcc.isEmpty()) { // tokenize number
-                    out.add(toInteger(digAcc));
-                    out.add(c);
-                } else {
-                    digAcc.append(c);
-                }
-            } else if (c >= '0' && c <= '9') {
-                digAcc.append(c);
-            } else {
-                if (!digAcc.isEmpty()) {
-                    if (digAcc.length() == 1 && digAcc.charAt(0) == '-') {
-                        // minus
-                        out.add(digAcc.charAt(0));
-                        digAcc.setLength(0);
-                    } else {
-                        // number
-                        out.add(toInteger(digAcc));
-                    }
-                }
-                out.add(c);
-            }
-        }
-        if (!digAcc.isEmpty()) { // tokenize number
-            out.add(toInteger(digAcc));
-        }
-        return out;
+    int applyCurrentOperation(char curOp, int leftV, int rightV) {
+        return leftV + (curOp == '-' ? -rightV : rightV);
     }
 
-    Integer toInteger(StringBuilder sb) {
-        Integer out = Integer.valueOf(sb.toString());
-        sb.setLength(0);
+    Integer getNumberFromAccumulator() {
+        if (numberAccum.isEmpty()) {
+            return 0;
+        }
+        Integer out = Integer.valueOf(numberAccum.toString());
+        numberAccum.setLength(0);
         return out;
     }
 
     @Test
     public void testFromSite() {
-        assertEquals(List.of('(', 4, '-', '(', 1, '+', '(', 3, ')', -2, ')', -5, ')'),
-                tokenize("(4-(1+(3)-2)-5)").stream().toList());
         assertEquals(2, calculate("(4-(0)-2)"));
         assertEquals(-3, calculate("(4-(2)-5)"));
         assertEquals(-3, calculate("(4-(4-2)-5)"));
@@ -105,6 +80,9 @@ public class BasicCalculator {
 
     @Test
     public void test() {
+        assertEquals(2, calculate("1+1"));
+        assertEquals(-1, calculate("-1"));
+        assertEquals(-2, calculate("-1-1"));
         assertEquals(-3, calculate("-1-1-1"));
         assertEquals(2, calculate("1 + 1"));
         assertEquals(3, calculate("4 - 1"));
@@ -112,10 +90,11 @@ public class BasicCalculator {
         assertEquals(3, calculate("-1+4"));
         assertEquals(3, calculate("1+1+1"));
         assertEquals(3, calculate(" 2-1 + 2 "));
-        assertEquals(23, calculate("(1+(4+5+2)-3)+(6+8)"));
-        assertEquals(3, calculate("5-(1+1)"));
         assertEquals(-2, calculate("-(1+1)"));
+        assertEquals(-1, calculate("-(1)"));
+        assertEquals(3, calculate("5-(1+1)"));
         assertEquals(-2, calculate("(-(1+1))"));
+        assertEquals(4, calculate("(5)-1"));
         assertEquals(3, calculate("(2+(3))-(1+1)"));
         assertEquals(-3, calculate("((2-3))-(1+1)"));
         assertEquals(0, calculate("(2-2)-((1-1))"));
@@ -125,16 +104,8 @@ public class BasicCalculator {
         assertEquals(2, calculate("(4-(1+(3)-2))"));
         assertEquals(-3, calculate("((1+(3)-2)-5)"));
         assertEquals(2, calculate("(4-(3-1))"));
+        assertEquals(23, calculate("(1+(4+5+2)-3)+(6+8)"));
     }
-    @Test
-    public void testTokenize() {
-        assertEquals(List.of(-123), tokenize("-123").stream().toList());
-        assertEquals(List.of('-', '(', 1, '+', 1, ')'), tokenize("-(1 + 1)").stream().toList());
-        assertEquals(List.of(1, '+', 1), tokenize("1 + 1").stream().toList());
-        assertEquals(List.of(2, '-', 1, '+', 2), tokenize(" 2-1 + 2 ").stream().toList());
-        assertEquals(List.of(-1, '-', 1, '-', 1), tokenize("-1-1-1").stream().toList());
-        assertEquals(List.of('(', 1, '+', '(', 4, '+', 5, '+', 2, ')', -3, ')', '+', '(', 6, '+', 8, ')'),
-                tokenize("(1+(4+5+2)-3)+(6+8)").stream().toList());
-    }
+
 
 }
