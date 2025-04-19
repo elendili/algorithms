c8dd9b5ec035cb2b6c4d6431b75961a7ad5230d7
elendili <elendilab@gmail.com>
2025-04-19 20:04:53 +0200

    3: src/main/java/leetcode/easy/FindAnagramMappings_760.java

--- src/main/java/leetcode/easy/FindAnagramMappings_760.java
+++ src/main/java/leetcode/easy/FindAnagramMappings_760.java
@@ -1,4 +1,23 @@
 package leetcode.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/find-anagram-mappings/?envType=study-plan-v2&envId=premium-algo-100
+ */
 public class FindAnagramMappings_760 {
+    public int[] anagramMappings(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        // hash nums2 indexes
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(nums2[i], i);
+        }
+        int[] out= new int[n];
+        for (int i = 0; i < n; i++) {
+            out[i] = map.get(nums1[i]);
+        }
+        return out;
+    }
 }
