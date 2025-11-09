94dd74af64a357f6e37a64b8554938978cc77bb0
elendili <elendilab@gmail.com>
2025-11-09 21:33:21 +0100

    up

--- src/main/java/leetcode/medium/SnakeGameDesign.java
+++ src/main/java/leetcode/medium/SnakeGameDesign.java
@@ -4,6 +4,25 @@ import java.util.LinkedHashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/*
+https://leetcode.com/problems/design-snake-game/description/?envType=study-plan-v2&envId=premium-algo-100
+Design a Snake game that is played on a device with screen size height x width. Play the game online if you are not familiar with the game.
+
+The snake is initially positioned at the top left corner (0, 0) with a length of 1 unit.
+
+You are given an array food where food[i] = (ri, ci) is the row and column position of a piece of food that the snake can eat. When a snake eats a piece of food, its length and the game's score both increase by 1.
+
+Each piece of food appears one by one on the screen, meaning the second piece of food will not appear until the snake eats the first piece of food.
+
+When a piece of food appears on the screen, it is guaranteed that it will not appear on a block occupied by the snake.
+
+The game is over if the snake goes out of bounds (hits a wall) or if its head occupies a space that its body occupies after moving (i.e. a snake of length 4 cannot run into itself).
+
+Implement the SnakeGame class:
+
+SnakeGame(int width, int height, int[][] food) Initializes the object with a screen of size height x width and the positions of the food.
+int move(String direction) Returns the score of the game after applying one direction move by the snake. If the game is over, return -1.
+ */
 public class SnakeGameDesign {
 
     class SnakeGame {
@@ -38,7 +57,7 @@ public class SnakeGameDesign {
                 return -1;
             }
             // remove tail if no food found
-            if (foodIndex<food.length && p.row == food[foodIndex][0] && p.col == food[foodIndex][1]) {
+            if (foodIndex < food.length && p.row == food[foodIndex][0] && p.col == food[foodIndex][1]) {
                 // found food
                 foodIndex++;
             } else {
@@ -63,6 +82,7 @@ public class SnakeGameDesign {
             assertEquals(expected[i], actual, i + ". Move " + moves[i]);
         }
     }
+
     @org.junit.jupiter.api.Test
     public void test2() {
         // [[2,2,[[0,1]]],["R"],["D"]]
@@ -78,10 +98,10 @@ public class SnakeGameDesign {
     @org.junit.jupiter.api.Test
     public void test3() {
         SnakeGame snakeGame = new SnakeGame(3, 3, new int[][]{
-                {2,0},{0,0},{0,2},{2,2}
+                {2, 0}, {0, 0}, {0, 2}, {2, 2}
         });
-        String[] moves = new String[]{"D","D","R","U","U","L","D","R","R","U","L","D"};
-        int[] expected = new int[]{0,1,1,1,1,2,2,2,2,3,3,3};
+        String[] moves = new String[]{"D", "D", "R", "U", "U", "L", "D", "R", "R", "U", "L", "D"};
+        int[] expected = new int[]{0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3};
         for (int i = 0; i < moves.length; i++) {
             int actual = snakeGame.move(moves[i]);
             assertEquals(expected[i], actual, i + ". Move " + moves[i]);
