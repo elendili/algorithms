19b9eb2609323159ac4cbd26abff3517e118e871
elendili <elendilab@gmail.com>
2025-11-09 20:19:30 +0100

    up

---
+++ src/main/java/leetcode/medium/TicTacToeDesign.java
@@ -0,0 +1,50 @@
+package leetcode.medium;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TicTacToeDesign {
+    class TicTacToe {
+        final int[] rows;
+        final int[] columns;
+        int diagonal;
+        int reverseDiagonal;
+        final int n;
+
+        public TicTacToe(int n) {
+            this.n = n;
+            rows = new int[n];
+            columns = new int[n];
+            diagonal = 0;
+            reverseDiagonal = 0;
+        }
+
+        public int move(int row, int col, int player) {
+            int toAdd = player == 1 ? 1 : -1;
+            rows[row] += toAdd;
+            if (Math.abs(rows[row]) == n) return player;
+            columns[col] += toAdd;
+            if (Math.abs(columns[col]) == n) return player;
+            if (row == col) { // top-left to bottom-right diagonal
+                diagonal += toAdd;
+                if (Math.abs(diagonal) == n) return player;
+            }
+            if (row + col == n - 1) { // top-right to bottom-left diagonal
+                reverseDiagonal  += toAdd;
+                if (Math.abs(reverseDiagonal) == n) return player;
+            }
+            return 0;
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    public void test() {
+        TicTacToe ticTacToe = new TicTacToe(3);
+        assertEquals(0, ticTacToe.move(0, 0, 1));
+        assertEquals(0, ticTacToe.move(0, 2, 2));
+        assertEquals(0, ticTacToe.move(2, 2, 1));
+        assertEquals(0, ticTacToe.move(1, 1, 2));
+        assertEquals(0, ticTacToe.move(2, 0, 1));
+        assertEquals(0, ticTacToe.move(1, 0, 2));
+        assertEquals(1, ticTacToe.move(2, 1, 1));
+    }
+}
