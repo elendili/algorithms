4405cb784ac22b3b4fe192329094aca5d14cca63
elendili <elendilab@gmail.com>
2026-01-02 12:24:34 +0100

    MissingElementInSortedArray_1060.java

---
+++ src/main/java/leetcode/medium/MissingElementInSortedArray_1060.java
@@ -0,0 +1,42 @@
+package leetcode.medium;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static collections.ArrayUtils.extract1dArrayFromBracketedString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MissingElementInSortedArray_1060 {
+    public int missingElement(int[] nums, int k) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+
+        while (left < right) {
+            int mid = right - (right - left) / 2;
+            if (nums[mid] - nums[0] - mid < k) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return nums[0] + k + left;
+    }
+
+    @ParameterizedTest
+    @CsvSource(useHeadersInDisplayName = true,
+            delimiter = '|',
+            textBlock = """
+                    expected | k   | nums
+                    2        | 1   | [1]
+                    3        | 1   | [1,2]
+                    5        | 1   | [4,7,9,10]
+                    8        | 3   | [4,7,9,10]
+                    6        | 3   | [1,2,4]
+                    """
+    )
+    public void test(int expected, int n, String nums) {
+        assertEquals(expected,
+                missingElement(extract1dArrayFromBracketedString(nums), n));
+    }
+}
